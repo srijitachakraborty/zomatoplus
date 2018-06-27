b1a133f8f4ad193b1a516fec899a40992439381c
srijitachakraborty <schakraborty563@gmail.com>
2018-06-27 13:31:26 +0530

    update

--- src/main/java/com/thinkxfactor/zomatoplus/controller/repository/RestuarantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thinkxfactor.zomatoplus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thinkxfactor.zomatoplus.models.Restaurant;
-
-public interface RestuarantRepository extends JpaRepository<Restaurant,Long> {
-
-}
